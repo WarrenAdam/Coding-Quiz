fa526fd807f2ed10b899ef71e4e285dde2bcfd82
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-05-29 20:12:43 -0400

    added array of questions.

---
+++ Assets/js/questions.js
@@ -0,0 +1,28 @@
+let MCQS = [{
+    question: "What does HTML stand for?",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Language",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+},
+
+{
+    question: "What does CSS stand for?",
+    choice1: "Colorful Style Sheet",
+    choice2: "Creative Style Sheet",
+    choice3: "Cascading Style Sheet",
+    choice4: "Computer Style Sheet",
+    answer: 2
+},
+
+{
+    question: "Which HTML tag is used to define and internal style sheet??",
+    choice1: "<style>",
+    choice2: "<script>",
+    choice3: "<html>",
+    choice4: "<h1>",
+    answer: 0
+},
+
+]
